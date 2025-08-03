2ba65dd043cdb99fc0d14584b08d8c67fce818ac
kevin0018 <akevin.2215@gmail.com>
2025-08-03 15:32:22 +0200

    Initialize WriteSphere backend with Maven setup and Spring Boot application

---
+++ backend/src/main/java/com/writesphere/writesphere/WritesphereApplication.java
@@ -0,0 +1,13 @@
+package com.writesphere.writesphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritesphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritesphereApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/writesphere/writesphere/WritesphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.writesphere.writesphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritesphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
